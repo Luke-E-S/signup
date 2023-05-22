ec1439e5faaedfb12ed8584d1e118e2c8043d411
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-22 08:03:45 +0100

    feat: Placeholder for firstname. Validation code also
    Add JS code to validate first name input

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const fname = document.querySelector("#first-name");
 
+fname.addEventListener("focusout",  () => validate());
+
+function validate() {
+    if(fname.value.length < 2) {
+        fname.className = "invalid";
+    } else {fname.className = "valid"};
+}
